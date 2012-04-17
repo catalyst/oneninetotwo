e31b0b0ec5c76cb2d9f5c07dd3488ec3f32afdbb
Aaron Wells <aaronw@catalyst.net.nz>
2012-04-18 11:36:46 +1200

    Make import_backup_file_silently() create the course, to retain more course settings
    The old way of creating the course first and then using import_backup_file_silently()
    to overwrite it, only overwrites the course contents but doesn't preserve any of the
    course settings except those that are copied over by hand later.
    
    By passing in a 0 for $courseid, you force import_backup_file_silently() to create the
    course, copying all the course settings over from the backup file. This is sort of an
    undocumented feature of the function which doesn't work in standard Moodle because it
    fails to create a context for the course. The associated change to onenine.patch fixes
    that problem, though.

--- bin/restore19course.php
+++ bin/restore19course.php
@@ -11,16 +11,6 @@ if (!is_file($_ENV['BACKUPFILE'])) {
     die ('Not a file');
 }
 
-
-$course = new stdClass();
-$course->category = 1;
-$course->fullname = 'Fake.1';
-$course->shortname = 'Fake.1';
-$course->idnumber = 'Fake.1';
-$course->format = 'weeks';
-$course->numsections = 60;
-
-
 $preferences = array('restore_groups' => true,
                      'restore_logs' => true,
                      'restore_messages' => true,
@@ -29,20 +19,4 @@ $preferences = array('restore_groups' => true,
                      'restore_site_files' => true,
                      'restore_metacourse' => true);
 
-if ($destcourse = create_course($course)) {
-    import_backup_file_silently($_ENV['BACKUPFILE'], $destcourse->id, true, true,$preferences);
-}
-
-// Sync the course to the backup file
-// HACK - peek at the info moodle backup restore keeps in the session
-
-$course = $destcourse;
-$course->fullname       = $SESSION->course_header->course_fullname;
-$course->summary        = $SESSION->course_header->course_summary;
-$course->idnumber       = $SESSION->course_header->course_idnumber;
-$course->shortname      = $SESSION->course_header->course_shortname;
-$course->numsections    = $SESSION->course_header->course_numsections;
-$course->format         = $SESSION->course_header->course_format;
-
-update_course(addslashes_object($course));
-?>
+import_backup_file_silently($_ENV['BACKUPFILE'], 0, true, true,$preferences);
